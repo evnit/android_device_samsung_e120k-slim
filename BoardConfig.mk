b1da89dc37e04e3a23607481818d277b1b0094de
evnit <evnithn@gmail.com>
2014-05-22 07:32:22 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,6 +37,8 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8660_SURF
 TARGET_OTA_ASSERT_DEVICE := SHV-E120K,e120k
 
 # Kernel
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom kgsl.mmutype=gpummu usb_id_pin_rework=true no_console_suspend=true zcache msm_watchdog.appsbark=0 msm_watchdog.enable=1 loglevel=4 androidboot.selinux=permissive
+BOARD_KERNEL_BASE := 0x48000000
 TARGET_KERNEL_CONFIG        := cyanogenmod_e120k_defconfig
 TARGET_KERNEL_SOURCE        := kernel/samsung/msm8660-common
 
