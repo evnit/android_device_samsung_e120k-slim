f4338c33e7242c3e25c7599f32402f1a92fb69fe
Le Anh Dung <evnithn@gmail.com>
2014-09-03 08:25:53 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -20,6 +20,12 @@ $(call inherit-product-if-exists, vendor/samsung/e120k/e120k-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+PRODUCT_PROPERTY_OVERRIDES += ro.sf.lcd_density=320
+
 # Ramdisk
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/ramdisk/init.qcom.usb.rc:root/init.qcom.usb.rc \
